ef21d27620ac8ea42614ff3b491b924ac58b7086
Dave Tamayo <djbtamayo@gmail.com>
2016-07-05 16:46:49 -0400

    Initial push of project. Implemented basic structure of main menu.

---
+++ Assets/Scripts/CanvasElementDrift.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to a Canvas element to have it drift in random directions
+/// but anchored to its starting location
+/// </summary>
+
+[DisallowMultipleComponent]
+public class CanvasElementDrift : MonoBehaviour {
+    	
+    [SerializeField] float driftSpeed = 0.4f;
+    [SerializeField] float maxDistance = 40;
+    [SerializeField] float minDriftTime = 1;
+    [SerializeField] float maxDriftTime = 4;
+
+    Vector3 startingPosition;
+    Vector3 direction;
+    Vector3 destination;
+
+
+    void Awake () {
+
+        startingPosition = this.GetComponent<RectTransform>().localPosition;
+        StartCoroutine(GetNewDestination());
+        if(minDriftTime >= maxDriftTime) { Debug.LogError("minDriftTime >= maxDriftTime!!!"); }
+    }
+
+    void Update () {
+
+        Vector3 currentPosition = this.GetComponent<RectTransform>().localPosition;
+        this.transform.GetComponent<RectTransform>().localPosition = Vector3.Lerp(currentPosition, destination, driftSpeed * Time.deltaTime);
+    }
+
+    IEnumerator GetNewDestination () {
+        
+        float angle = Random.Range(0f, 360f);
+        float newDistance = Random.Range(0f, maxDistance);
+        float newX = newDistance * Mathf.Sin(angle) + startingPosition.x;
+        float newY = newDistance * Mathf.Cos(angle) + startingPosition.y;
+        destination = new Vector3(newX, newY, 0);
+
+        yield return new WaitForSeconds(Random.Range(minDriftTime, maxDriftTime));
+
+        StopCoroutine(GetNewDestination());
+        StartCoroutine(GetNewDestination());
+    }
+}
---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+/// <summary>
+/// Responsible for the movement of the camera as it moves from canvas to canvas
+/// </summary>
+
+[DisallowMultipleComponent]
+public class HUDController : MonoBehaviour {
+
+    [Header("Parameters")]
+    [SerializeField] float cameraMoveSpeed;
+    [SerializeField] float cameraRotationSpeed;
+
+    [Header("References")]
+    [SerializeField] Button[] HUDButtons;
+
+    Transform destination;
+
+
+    void Start () {
+
+        destination = Camera.main.transform;
+        HUDButtons[0].GetComponentInChildren<Text>().fontStyle = FontStyle.Bold;
+    }
+
+    void Update () {
+
+        if(Vector3.Distance(Camera.main.transform.position, destination.position) > 0.5f) {
+            
+            Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, destination.position, cameraMoveSpeed * Time.deltaTime);
+        }
+
+        if(Vector3.Angle(Camera.main.transform.forward, destination.forward) > 1) {
+
+            Camera.main.transform.rotation = Quaternion.Slerp(Camera.main.transform.rotation, destination.rotation, Time.deltaTime * cameraRotationSpeed);
+        }
+    }
+
+    // Called by the buttons on the bottom panel of the HUD Canvas
+    public void SetNewDestination (Transform newDestination) {
+
+        BoldActiveButton();
+        destination = newDestination;
+        //Camera.main.transform.eulerAngles = newDestination.eulerAngles;
+    }
+
+    void BoldActiveButton () {
+
+        foreach (Button button in HUDButtons) {
+
+            button.GetComponentInChildren<Text>().fontStyle = FontStyle.Normal;
+        }
+
+        EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().fontStyle = FontStyle.Bold;
+    }
+}
