f3244a6ac41d79b04f02d9fe46f76334220d6db4
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2016-07-06 15:45:06 -0400

    Added in text loading and made a scene for myself.

---
+++ Assets/Scripts/Text Loading/LoadLanguage.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class LoadLanguage : MonoBehaviour {
+
+    [Tooltip("The uiComponents must be entered into this lidt in the SAME ORDER that they are beiing read from the file - see text file to avoid confusion.")]
+    [SerializeField] List <Text> uiComponents = new List<Text>();
+
+    bool english = true;
+
+    public TextAsset englishText;
+    public TextAsset frenchText;
+
+    private string wholeFileAsOneString;
+    private List<string> eachLine = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ToggleLanguage() {
+        english = !english;
+
+        if (english) { LoadEnglish(); }
+        else { LoadFrench(); }
+    }
+
+    void LoadEnglish() {
+        wholeFileAsOneString = englishText.text;
+        FillUI();
+    }
+
+    void LoadFrench() {
+        wholeFileAsOneString = frenchText.text;
+        FillUI();
+    }
+
+    void FillUI() {
+        eachLine.Clear();
+        eachLine.AddRange(wholeFileAsOneString.Split("\n"[0]));
+
+        // Now fill the text fields 
+        for (int i = 0; i < eachLine.Count; i++)
+        {
+            uiComponents[i].text = eachLine[i];
+        }
+    }
+}
---
+++ Assets/Scripts/Text Loading/UIText.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour {
+
+    [Header("Profile UI Elements")]
+    [SerializeField] Text profileHeaderUI;
+    [SerializeField] Text avatarButtonUI;
+    [SerializeField] Text settingsButtonUI;
+    [SerializeField] Text accountButtonUI;
+
+    [Header("Classroom UI Elements")]
+    [SerializeField] Text classHeaderUI;
+    [SerializeField] Text messageBoardButtonUI;
+    [SerializeField] Text statsButtonUI;
+    [SerializeField] Text studentsButtonUI;
+
+    [Header("Ask A Tutor UI Elements")]
+    [SerializeField] Text tutorHeaderUI;
+    [SerializeField] Text callTutorButtonUI;
+    [SerializeField] Text textTutorButtonUI;
+	
+    [Header("Games UI Elements")]
+    [SerializeField] Text gamesHeaderUI;
+    [SerializeField] Text[] gameButtonUI;
+
+    [Header("Classroom UI Elements")]
+    [SerializeField] Text challengeUI1;
+    [SerializeField] Text challengeUI2;
+    [SerializeField] Text notificationsUI1;
+    [SerializeField] Text notificationsUI2;
+
+    // Setters
+    public void SetProfileHeaderUI(string newText) { profileHeaderUI.text = newText; }
+    public void SetAvatatButtonUI(string newText) { avatarButtonUI.text = newText; }
+    public void SetSettingButtonUI(string newText) { settingsButtonUI.text = newText; }
+    public void SetAccuontButtonUI(string newText) { accountButtonUI.text = newText; }
+
+    public void SetClassroomHeaderUI(string newText) { classHeaderUI.text = newText; }
+    public void SetMessageBoardButtonUI(string newText) { messageBoardButtonUI.text = newText; }
+    public void SetStatsButtonUI(string newText) { statsButtonUI.text = newText; }
+    public void SetStudentsButtonUI(string newText) { studentsButtonUI.text = newText; }
+
+    public void SetTutorHeaderUI(string newText) { classHeaderUI.text = newText; }
+    public void SetCallTutorButtonUI(string newText) { callTutorButtonUI.text = newText; }
+    public void SetTextTutorButtonUI(string newText) { textTutorButtonUI.text = newText; }
+}
