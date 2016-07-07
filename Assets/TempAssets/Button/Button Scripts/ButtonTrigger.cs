aebf5914f73e0c70c10e9689c346d26fddc7e69c
John Wesley 3 <johnwesley.livingston@gmail.com>
2016-07-07 18:25:46 -0400

    Added some dope buttons and animations. <3

---
+++ Assets/TempAssets/Button/Button Scripts/ButtonTrigger.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    public int trigger = 9;
+    public ButtonAnimate buttonAnimate;
+
+
+    void Start()
+    {
+
+    }
+
+
+    // Triggers animations on click
+    void OnMouseDown()
+    {
+        // TOP LEFT
+        if (gameObject.tag == "button_topleft")
+        {
+            buttonAnimate.trigger = 0;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("TopLeftClick");
+        }
+
+        // TOP MIDDLE
+        else if (gameObject.tag == "button_topmid")
+        {
+            buttonAnimate.trigger = 1;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("TopMidClick");
+        }
+
+        // TOP RIGHT
+        else if (gameObject.tag == "button_topright")
+        {
+            buttonAnimate.trigger = 2;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("TopRightClick");
+        }
+
+        // LEFT
+        else if (gameObject.tag == "button_left")
+        {
+            buttonAnimate.trigger = 3;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("LeftClick");
+        }
+
+        // MIDDLE
+        else if (gameObject.tag == "button_mid")
+        {
+            buttonAnimate.trigger = 4;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("MidClick");
+        }
+
+        // RIGHT
+        else if (gameObject.tag == "button_right")
+        {
+            buttonAnimate.trigger = 5;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("RightClick");
+        }
+
+        // BOTTOM LEFT
+        else if (gameObject.tag == "button_botleft")
+        {
+            buttonAnimate.trigger = 6;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("BotLeftClick");
+        }
+
+        // BOTTOM MIDDLE
+        else if (gameObject.tag == "button_botmid")
+        {
+            buttonAnimate.trigger = 7;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("BotMidClick");
+        }
+
+        // BOTTOM RIGHT
+        else if (gameObject.tag == "button_botright")
+        {
+            buttonAnimate.trigger = 8;
+            buttonAnimate.forceMyPies = true;
+            Debug.Log("BotRightClick");
+
+
+        }
+    }
+
+
+}
