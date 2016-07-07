c02c8a17604b5f90399b1ec08dadc9b8b26bf6a9
Dave Tamayo <djbtamayo@gmail.com>
2016-07-07 19:04:52 -0400

    Finished up the basic structure of Tutor submenu.

---
+++ Assets/Scripts/Menus/CallTest.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CallTest : MonoBehaviour {
+
+	
+    public void CallTutor () {
+
+        Application.OpenURL("tel:14168316769");
+    }
+}
