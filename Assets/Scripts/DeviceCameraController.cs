b31d7f5ce5768200ee2975380827220175df89e9
Rebecca <rebeccamitchell88@gmail.com>
2016-07-11 15:16:17 -0400

    Access Camera on iPhone

---
+++ Assets/Scripts/DeviceCameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviceCameraController : MonoBehaviour {
+
+	[SerializeField] WebCamTexture mCamera = null;
+	[SerializeField] GameObject plane;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Script has been started.");
+		plane = GameObject.FindGameObjectWithTag ("Player");
+
+		mCamera = new WebCamTexture ();
+		plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+		mCamera.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
