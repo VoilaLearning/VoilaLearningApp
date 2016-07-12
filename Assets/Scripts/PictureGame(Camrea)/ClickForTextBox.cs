cbe71fd8ebe7f00189a5c537c9f0de50b3d10597
Rebecca <rebeccamitchell88@gmail.com>
2016-07-12 18:07:55 -0400

    Almost finished up UI and mechanic of this game. Just need to hook it up to the etc plugin to access device cameras.

--- Assets/Scripts/PictureGame(Camrea)/ClickForTextBox.cs
+++ Assets/Scripts/PictureGame(Camrea)/ClickForTextBox.cs
@@ -5,37 +5,21 @@ using UnityEngine.UI;
 
 public class ClickForTextBox : MonoBehaviour, IPointerClickHandler {
 
+	[SerializeField] PictureWordGame pictureWordGame;
 	[SerializeField] InputField textInput;
 
-	RectTransform CanvasRect;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void OnPointerClick(PointerEventData eventData){
+		// Find all other text box
+		InputField[] otherFields = GameObject.FindObjectsOfType<InputField>();
+		// close them 
+		for (int i = 0; i < otherFields.Length; i++){
+			otherFields [i].GetComponent<InputFieldController> ().FinishInput ();	
+		}
 
-		Debug.Log ("Clicked here(World Pos): " + Input.mousePosition);
-		Debug.Log("Clicked here(Canvas Pos): " + Camera.main.WorldToViewportPoint(Input.mousePosition));
+		pictureWordGame.ShowButton ();
 		InputField newInput = Instantiate (textInput, Vector3.zero, Quaternion.identity) as InputField;
-
-
 		newInput.transform.SetParent (this.transform.parent);
-		//newInput.transform.position = Input.mousePosition;
 		newInput.GetComponent<RectTransform> ().position = Input.mousePosition;
 		newInput.transform.localScale = Vector3.one;
-		// newInput.GetComponent<RectTransform> ().localPosition = Camera.main.WorldToViewportPoint (Input.mousePosition);
-		// newInput.GetComponent<RectTransform> ().anchoredPosition = mousePos;
-	}
-
-	void CreateInputField(){
-
-
 	}
 }
