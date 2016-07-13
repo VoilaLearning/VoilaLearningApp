84b4061714538f3ded57c27cd39e1d0cb1c7bfb2
Dave Tamayo <djbtamayo@gmail.com>
2016-07-13 17:51:05 -0400

    Set up the main screen of the 21 Questions game

---
+++ Assets/Scripts/TwentyOneQuestions/TwentyOneQuestionsController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class TwentyOneQuestionsController : MonoBehaviour {
+
+    [SerializeField] GameObject[] activeGames;
+    [SerializeField] GameObject gamePrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TwentyOneQuestions/TwentyOneQuestionsGame.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+[DisallowMultipleComponent]
+public class TwentyOneQuestionsGame : MonoBehaviour {
+
+    [SerializeField] Button yesButton;
+    [SerializeField] Button noButton;
+    [SerializeField] GameObject sentMessage;
+    bool playersTurn = true;
+
+    public void SendYes () {
+
+        sentMessage.SetActive(true);
+        StopCoroutine("CloseGame");
+        StartCoroutine("CloseGame");
+    }
+
+    public void SendNo () {
+
+        sentMessage.SetActive(true);
+        StopCoroutine("CloseGame");
+        StartCoroutine("CloseGame");
+    }
+
+    IEnumerator CloseGame () {
+
+        yield return new WaitForSeconds(2);
+        this.gameObject.SetActive(false);
+        playersTurn = false;
+    }
+
+    public bool IsPlayersTurn () {
+
+        return playersTurn;
+    }
+
+    public void SetPlayersTurn (bool turn) {
+
+        playersTurn = turn;
+    }
+}
